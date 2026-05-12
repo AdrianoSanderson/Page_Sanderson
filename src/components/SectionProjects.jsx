cf3054eabbd0a1dd0353ab1ab9b87a3846f326a3
Adriano Sanderson <adrianoramos06@gmail.com>
2026-05-12 12:30:19 -0300

    Melhoria de descrição dos projetos.

--- src/components/SectionProjects.jsx
+++ src/components/SectionProjects.jsx
@@ -12,7 +12,9 @@ export default function () {
                 <div className="flex flex-col items-center border-solid border-2 border-sky-600 w-80 h-96 m-4 rounded-xl lg:w-80 delay-75 hover:bg-sky-600">
                     <h1 className="text-3xl	text-slate-200 max-sm:text-2xl max-sm:text-center">Portifólio</h1>
 
-                    <p className="text-base m-3 text-justify max-sm:text-sm text-slate-200">Nesse Projeto estou desenvolvendo um Portifólio com React, Tailwind e Vite. Onde posso apresentar meus projetos e habilidades.</p>
+                    <p className="text-base m-3 text-justify max-sm:text-sm text-slate-200">
+                        Portfólio pessoal desenvolvido com React, Vite e Tailwind CSS. Focado em alta performance, design responsivo e arquitetura limpa para apresentar minha trajetória e projetos Full Stack.
+                    </p>
 
                     <img className="mt-2 w-64 h-40 object-contain" src={portifolioImg} alt="img" />
                     <div className='mt-2'>
@@ -25,7 +27,9 @@ export default function () {
                 <div className="flex flex-col items-center border-solid border-2 border-sky-600 w-80 h-96 m-4 rounded-xl lg:w-80 delay-75 hover:bg-sky-600">
                     <h1 className="text-3xl	text-slate-200 max-sm:text-2xl max-sm:text-center">FaceExpression</h1>
 
-                    <p className="text-base m-3 text-justify max-sm:text-sm text-slate-200">Esse projeto é uma ferramenta de reconhecimento de expressões faciais. Que pode auxiliar Psicologos na avaliação de pacientes.</p>
+                    <p className="text-base m-3 text-justify max-sm:text-sm text-slate-200">
+                        Ferramenta para reconhecimento de expressões faciais validada por psicólogo. Utiliza React, Face-api e ApexCharts para detecção em tempo real e geração de gráfico quantitativo para análise de dados.
+                    </p>
 
                     <img className="mt-2 w-64 h-40 object-contain" src={faceExpressionImg} alt="img" />
                     <div className='mt-2'>
