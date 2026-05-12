bfb5c370c169b402330a81531ab9ce90b1d7b9ff
Adriano Sanderson <adrianoramos06@gmail.com>
2026-05-12 11:01:54 -0300

    Atualiza seção "Habilidades Técnicas"

--- src/components/DataSkills.js
+++ src/components/DataSkills.js
@@ -1,13 +1,15 @@
 import html from '../assets/images/html_img.svg'
 import css from '../assets/images/css_img.svg'
 import javascript from '../assets/images/js_img.svg'
+import vue from '../assets/images/vue_img.svg'
+import quasar from '../assets/images/quasar_img.svg'
+import nuxt from '../assets/images/nuxt_img.svg'
 import react from '../assets/images/react_img.svg'
 import git from '../assets/images/git_img.svg'
 import tailwind from '../assets/images/tailwind_img.svg'
 import node from '../assets/images/node_img.svg'
-import mongodb from '../assets/images/mongodb_img.svg'
+import nest from '../assets/images/nest_img.svg'
 import postgresql from '../assets/images/postgresql_img.svg'
-import prisma from '../assets/images/prisma_img.svg'
 
 const dataSkills = [
     {
@@ -23,32 +25,40 @@ const dataSkills = [
         img: javascript
     },
     {
-        name: "React JS",
-        img: react
+        name: "Vue",
+        img: vue
     },
     {
-        name: "Git",
-        img: git
+        name: "Quasar",
+        img: quasar
+    },
+    {
+        name: "Nuxt",
+        img: nuxt
+    },
+    {
+        name: "React",
+        img: react
     },
     {
         name: "Tailwind",
         img: tailwind
     },
     {
-        name: "Node JS",
+        name: "Node",
         img: node
     },
     {
-        name: "MongoDB",
-        img: mongodb
+        name: "Nest",
+        img: nest
     },
     {
         name: "Postgresql",
         img: postgresql
     },
     {
-        name: "Prisma",
-        img: prisma
-    }
+        name: "Git",
+        img: git
+    },
 ]
 export default dataSkills;
