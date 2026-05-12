b7a55bf50202c35001c46c2e8fc3d5b929d01944
Adriano Sanderson <adrianoramos06@gmail.com>
2026-05-12 11:47:19 -0300

    Melhora seção "Início"

--- src/components/SectionHello.jsx
+++ src/components/SectionHello.jsx
@@ -12,27 +12,26 @@ export default function SectionHello() {
                     </h3>
                     
                     <p className="text-2xl text-justify max-sm:text-xl mb-4">
-                        Estudante de Sistemas de Informação e apaixonado por tecnologia, atuo como desenvolvedor Full Stack focado em transformar ideias em experiências digitais inovadoras. Busco aprimorar constantemente minhas habilidades para criar interfaces intuitivas e funcionais, garantindo uma usabilidade fluida e envolvente em cada projeto, desde landing pages até plataformas completas.
+                        Especialista no desenvolvimento de soluções para os setores bancário, financeiro e de compliance. Com domínio técnico no ecossistema Vue.js e arquiteturas em Node/NestJS, atuo na construção de plataformas de alta performance. Além da experiência em sistemas corporativos, desenvolvo projetos personalizados como freelancer, entregando sempre usabilidade e inovação. Graduado em Sistemas de Informação pela UFPA.
                     </p>
-
-                    <div>
-                        <button 
-                            className="bg-zinc-300 w-28 text-sky-600 h-7 rounded-md m-2 hover:bg-neutral-800"
-                            onClick={() => { window.open("https://www.linkedin.com/in/adrianosanderson/", "_blank") }}
-                        >
-                            LinkedIn
-                        </button>
-
-                        <button 
-                            className="bg-zinc-300 w-28 text-sky-600 h-7 rounded-md m-2 hover:bg-neutral-800"
-                            onClick={() => { window.open("https://github.com/AdrianoSanderson", "_blank") }}
-                        >
-                            Github
-                        </button>
-                    </div>
                 </div>
-                <div className="w-6/12 flex justify-end max-md:w-full max-md:justify-center">
-                    <img className="h-96 max-sm:h-80" src="profile.png" alt="profile" />
+                <div className="w-6/12 flex flex-col items-center max-md:w-full max-md:justify-center">
+                    <img className="w-96 h-96 max-sm:h-80 w-80" src="profile.png" alt="profile" />
+                    <div className="mt-8">
+                        <button 
+                                className="bg-zinc-300 w-28 text-sky-600 h-7 rounded-md m-2 hover:bg-neutral-800"
+                                onClick={() => { window.open("https://www.linkedin.com/in/adrianosanderson/", "_blank") }}
+                            >
+                                LinkedIn
+                            </button>
+
+                            <button 
+                                className="bg-zinc-300 w-28 text-sky-600 h-7 rounded-md m-2 hover:bg-neutral-800"
+                                onClick={() => { window.open("https://github.com/AdrianoSanderson", "_blank") }}
+                            >
+                                Github
+                            </button>
+                    </div>
                 </div>
             </div>
         </section>
